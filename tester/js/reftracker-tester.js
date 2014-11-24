2eb899300176ca4503b3f3d014bccc0d183942e7
lukasz <lukasz.frankowski@gmail.com>
2014-11-24 22:16:55 +0100

    tester template

---
+++ tester/js/reftracker-tester.js
@@ -0,0 +1,12 @@
+/**
+ * Angular Reftracker Tester module
+ *
+ * @author lukasz.frankowski@gmail.com
+ */
+
+var rtTester = angular.module('rtTester', ['refTracker', 'ngResource']);
+
+rtTester.controller('listController',
+['$resource',
+function($resource) {
+}]);
