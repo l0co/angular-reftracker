f45d2ddaaf6d5edb0e59598cfdd25d11b14f6173
lukasz <lukasz.frankowski@gmail.com>
2014-11-26 17:09:30 +0100

    reference tracker template code

--- src/angular-reftracker.js
+++ src/angular-reftracker.js
@@ -31,6 +31,9 @@
 
 var refTracker = angular.module('refTracker', []);
 
+/**
+ * References cache
+ */
 refTracker.provider('refCache', function() {
 
     // default id resolver working with UUID id-s in "id" object field
@@ -51,9 +54,85 @@ refTracker.provider('refCache', function() {
 
     this.$get = [
         function() {
-            // TODOLF impl
-            return {};
+            return new function RefCache() {
+                var cache = {};
+
+                function CacheEntry(id) {
+                    this.id = id;
+                    this.refCount = 0;
+                    this.reference = null;
+                }
+
+                function isObject(object) {
+                    return object && typeof object === "object" && !Array.isArray(object);
+                }
+
+                /**
+                 * Adds new object reference to cache
+                 * @param {object} object Object to add (will be added recursively)
+                 * @param {string} method If the reference is already created, defines the method of reference update
+                 *                        operation, that may be 'merge' (will merge new objects into old, default) or
+                 *                        'replace' (will clean the previous references and use only new object
+                 *                        properties).
+                 * @returns {object} referenced object
+                 */
+                this.addReference = function(object, method) {
+                    method = method || 'merge';
+
+                    // TODOLF impl
+                    console.log('add reference', object);
+                };
+
+                /**
+                 * Removes the object reference from cache
+                 * @param {object} object Object to remove (will be removed recursively)
+                 */
+                this.removeReference = function(object) {
+                    // TODOLF impl
+                    console.log('remove reference', object);
+                }
+
+            };
         }
     ];
 
 });
+
+/**
+ * Managed references scope
+ */
+refTracker.factory('refScope', ['refCache',
+    function(refCache) {
+
+        /**
+         * Managed references scope constructor
+         * @param {object} $scope scope to join
+         */
+        return function RefScope($scope) {
+            this.referenced = [];
+
+            var $this = this;
+
+            // remove all references when scope dies
+            $scope.$on('$destroy', function() {
+                angular.forEach($this.referenced, function(object) {
+                    refCache.removeReference(object);
+                });
+            });
+
+            /**
+             * Function for adding objects to $scope
+             * @param {string} prop After execution the object appears in $scope.prop
+             * @param {object} object Object to add to $scope
+             */
+            this.add = function(prop, object) {
+                // TODOLF what if the existing scope object is replaced? remove prop from refCache and referenced first (!)
+                var reference = refCache.addReference(object);
+                $this.referenced.push(reference);
+                $scope[prop] = reference;
+            }
+
+        }
+
+    }
+]);
