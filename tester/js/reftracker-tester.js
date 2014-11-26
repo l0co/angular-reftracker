7cd33bca29f1b0c2a3ca46e3ced394b3be2b9388
lukasz <lukasz.frankowski@gmail.com>
2014-11-26 15:16:12 +0100

    hard reloads

--- tester/js/reftracker-tester.js
+++ tester/js/reftracker-tester.js
@@ -10,21 +10,36 @@ rtTester.controller('listController',
 ['$scope', '$resource',
 function($scope, $resource) {
 
-    $scope.items = [];
-
-    $resource('json/objects.json').query({}, function(data) {
-        $scope.list = data;
-    });
-
     $scope.add = function(item) {
         $scope.items.push(item);
-    }
+    };
+
+    $scope.hardReload = function() {
+        $resource('json/objects.json').query({}, function(data) {
+            $scope.list = data;
+        });
+    };
+
+    $scope.reset = function(item) {
+        $scope.items = [];
+        $scope.hardReload();
+    };
+
+    $scope.reset();
 
 }]);
 
 rtTester.controller('editController',
 ['$scope', '$resource',
 function($scope, $resource) {
+
     // item goes from parent scope here, we use it to create separate reference in this controller
     $scope.element = $scope.item;
+
+    $scope.hardReload = function() {
+        $resource($scope.element.link).get({}, function(data) {
+            $scope.element = data;
+        });
+    };
+
 }]);
